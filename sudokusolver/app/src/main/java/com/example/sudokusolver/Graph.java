ae771bcef0c6245724cb3bf012e7339fa6c95a58
HualiangZ <zhualiang88@gmail.com>
2023-03-28 11:18:00 +0100

    created edges for each rows and columns

--- sudokusolver/app/src/main/java/com/example/sudokusolver/Graph.java
+++ sudokusolver/app/src/main/java/com/example/sudokusolver/Graph.java
@@ -4,66 +4,66 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Graph {
-//    HashMap<Integer, Object> map;
-//    private int nodeId;
+    HashMap<Integer, Object> map;
+    private int nodeId;
 
     private int selectRow;
     private int selectCol;
     int[][] board;
 
-//    ArrayList<Integer> neighbour[];
+    ArrayList<Integer>[] nodeNeighbour;
 
     Graph(){
         selectRow = 0;
         selectCol = 0;
-//        nodeId = 1;
+        nodeId = 1;
         board = new int[9][9];
 
 
-//        neighbour = new ArrayList[81];
-//        for (int i=1; i<82; ++i)
-//            neighbour[i] = new ArrayList();
-//
-//        map = new HashMap<>();
-//
-//        for(int row=0; row<9; row++){
-//            for(int col=0; col<9; col++){
-//                board[row][col] = 0;
-//                map.put(nodeId,board[row][col]);
-//                nodeId++;
-//            }
-//        }
+        nodeNeighbour = new ArrayList[81];
+        for (int i=0; i<81; ++i)
+            nodeNeighbour[i] = new ArrayList();
+
+        map = new HashMap<>();
+
+        for(int row=0; row<9; row++){
+            for(int col=0; col<9; col++){
+                board[row][col] = 0;
+                map.put(nodeId,board[row][col]);
+                nodeId++;
+            }
+        }
 
 
 
     }
 
-//    public void addEdges(int num, int nextNum){
-//        neighbour[num].add(nextNum);
-//        neighbour[nextNum].add(num);
-//    }
-//
-//    public void edges(){
-//        int row = 1;
-//        //add edges for each row
-//        for(int idx=1; idx<82; idx++){
-//            if(idx != row){
-//                this.addEdges(row,idx);
-//            }
-//            if(idx%9 == 0){
-//                row=row+9;
-//            }
-//        }
-//
-//        //add edges for each column
-//        for(int col=1; col<10; col++){
-//            for(int idx=col; idx<82; idx=idx+9){
-//                if(idx != col){
-//                    this.addEdges(col, idx);
-//                }
-//            }
-//        }
-//    }
+    public void addEdges(int num, int nextNum){
+        nodeNeighbour[num].add(nextNum);
+        nodeNeighbour[nextNum].add(num);
+    }
+
+    public void createEdges(){
+        int row = 0;
+        //create edges for each row
+        for(int idx=0; idx<81; idx++){
+            if(idx != row){
+                this.addEdges(row,idx);
+            }
+            if((idx+1)%9 == 0){
+                row=row+9;
+            }
+        }
+
+        //create edges for each column
+        for(int col=0; col<9; col++){
+            for(int idx=col; idx<81; idx=idx+9){
+                if(idx != col){
+                    this.addEdges(col, idx);
+                }
+            }
+        }
+    }
 
 
     public void addNum(int num){
